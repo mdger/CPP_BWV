8ff1f72a45e3b4c85cc47345c2d7a168fd6ad65c
mdger <marco.dung@gmx.de>
2017-08-31 21:09:58 +0200

    Initial Commit

---
+++ ae_bwv_sophomore/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ae_bwv_sophomore
+//
+//  Created by Marco Dung on 31.08.17.
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
